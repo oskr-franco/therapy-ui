52154d44c34b3fbf075098a1f7be82962a7cf2bf
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-06-18 22:23:42 -0600

    adding component to modal

--- src/components/templates/Exercises/Exercises.jsx
+++ src/components/templates/Exercises/Exercises.jsx
@@ -1,13 +1,24 @@
 
-import React from 'react';
+
+
+import React, { useCallback } from 'react';
+import { FaPlusCircle } from "react-icons/fa";
 
 import ExerciseCard from '@/components/ExerciseCard'
-import {withOpenModal} from '@/hocs/withOpenModal';
+import ExerciseForm from '@/components/ExerciseForm'
+import { withOpenModal } from '@/hocs/withOpenModal';
 
-function Exercises({ exercises, openModal}) {
+import styles from './Exercises.module.scss';
+
+function Exercises({ exercises, openModal }) {
+  const onCreateExerciseHandler = useCallback(() => {
+    openModal({
+      component: ExerciseForm,
+    });
+  }, [openModal]);
   return (
     <>
-      <button onClick={openModal}>Click me</button>
+      <FaPlusCircle alt="Agregar" onClick={onCreateExerciseHandler} className={styles.addBtn} />
       <div>
         {exercises.map((exercise) => (
           <ExerciseCard key={exercise.id} {...exercise} />
