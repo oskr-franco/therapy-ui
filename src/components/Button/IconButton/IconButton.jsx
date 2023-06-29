6f827652c6f20e00fbb0c85e1cbaefe6fe92158d
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-06-28 19:23:43 -0600

    adding tooltip

--- src/components/Button/IconButton/IconButton.jsx
+++ src/components/Button/IconButton/IconButton.jsx
@@ -4,10 +4,11 @@ import PropTypes from "prop-types";
 
 import styles from "./IconButton.module.scss";
 
-function IconButton({ className, icon: Icon, ...props }) {
+function IconButton({ className, icon: Icon, tooltip, tooltipPosition, ...props }) {
   return (
     <button className={cx(styles.button, className)} {...props}>
       <Icon />
+      {tooltip && <div className={cx(styles.tooltip, styles[tooltipPosition])}>{tooltip}</div>}
     </button>
   );
 }
@@ -15,6 +16,12 @@ function IconButton({ className, icon: Icon, ...props }) {
 IconButton.propTypes = {
   className: PropTypes.string,
   icon: PropTypes.elementType.isRequired,
+  tooltip: PropTypes.string,
+  tooltipPosition: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+};
+
+IconButton.defaultProps = {
+  tooltipPosition: 'top',
 };
 
 export default React.memo(IconButton);
